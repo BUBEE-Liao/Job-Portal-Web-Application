80f12626de644c1863df42dfd067c9220673b3ae
BUBEE-Liao <95071898+BUBEE-Liao@users.noreply.github.com>
2024-12-25 00:14:03 +0800

    Delete jobportal directory

--- jobportal/src/main/java/com/bubee/jobportal/repository/RecruiterProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bubee.jobportal.repository;
-
-import com.bubee.jobportal.entity.RecruiterProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecruiterProfileRepository extends JpaRepository<RecruiterProfile, Integer> {
-}
--- jobportal/src/main/java/com/bubee/jobportal/repository/UsersTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bubee.jobportal.repository;
-
-import com.bubee.jobportal.entity.UsersType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersTypeRepository extends JpaRepository<UsersType, Integer> {
-}
